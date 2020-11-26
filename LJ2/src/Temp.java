14a1f23e595c926560ed720c9602b0d420262b09
Scott Underwood <59848341+Ra5hma@users.noreply.github.com>
2020-11-25 18:00:56 -0600

    Add files via upload

---
+++ LJ2/src/Temp.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+
+public enum Temp {
+    C (0, "Celsius"),
+    F (1, "Fahrenheit"),
+    K (2, "Kelvin"), ;
+
+    private final float tempCode;
+    private final String description;
+
+        Temp (int tempCode, String description){
+            this.tempCode = tempCode;
+            this.description = description;
+        }
+    public float getTempCode(){
+            return this.tempCode;
+    }
+    public String getDescription(){
+            return this.description;
+    }
+
+    public static void outputTemp(Temp theTemp){
+
+        switch(theTemp) {
+            case C:
+                System.out.println("If the degrees in Celsius are " + "then the Kelvin units would be " + "and the " +
+                        "Fahrenheit temperature would be " );
+        }
+    }//comment made to test Git!
+}
